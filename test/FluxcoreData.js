3c4df9e74cee4f94b20f050f6f04405ac9c7cf0d
LynxyssCZ <LynxyssCZ@gmail.com>
2015-11-28 07:28:05 +0100

    Prepare test suites

---
+++ test/FluxcoreData.js
@@ -0,0 +1,25 @@
+module.exports.stores = {
+	load: [{
+		store: 'Override', type: 'Override.load',
+		payload: {}, expect: {}
+	}],
+	create: [{
+		store: 'Override', type: 'Override.create',
+		payload: {}, expect: {}
+	}],
+	update: [{
+		store: 'Override', type: 'Override.update',
+		payload: {}, expect: {}
+	}],
+	remove: [{
+		store: 'Override', type: 'Override.remove',
+		payload: {}, expect: {}
+	}]
+};
+
+module.exports.actions = {
+	load: [],
+	create: [],
+	update: [],
+	remove: []
+};
