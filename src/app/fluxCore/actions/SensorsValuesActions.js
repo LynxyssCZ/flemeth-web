31a63c1401ab2f3acbe1fb46e1f7d8e1b119b51e
LynxyssCZ <LynxyssCZ@gmail.com>
2015-11-28 14:16:14 +0100

    Snapshots load actions
    Prepared for future caching

--- src/app/fluxCore/actions/SensorsValuesActions.js
+++ src/app/fluxCore/actions/SensorsValuesActions.js
@@ -1,4 +1,57 @@
-// TODO
-module.exports = {
+var Hash = require('object-hash');
 
+module.exports = {
+	load: function(fromTs, toTs, zones) {
+		var requestMeta = {
+			time: Date.now(),
+			hash: Hash.sha1({
+				to: toTs,
+				from: fromTs,
+				zones: zones
+			})
+		};
+
+		if (zones && !Array.isArray(zones)) {
+			zones = [zones];
+		}
+
+		return this.api.get('sensorsvalues/', {
+			to: toTs,
+			from: fromTs,
+			zones: zones ? zones.join(';') : undefined
+		}).then(function(result) {
+			return {
+				sensorsValues: groupValues(result.sensorsValues, getSensorKey, function(key, values) {
+					return {
+						zoneId: key,
+						values: values,
+						meta: requestMeta
+					};
+				})
+			};
+		});
+	}
 };
+
+function getSensorKey(sensorValue) {
+	return sensorValue.sensorId;
+}
+
+function groupValues(values, getKey, buildBundle) {
+	var heap = values.reduce(function(heap, value) {
+		var key = getKey(value);
+
+		if (heap.hasOwnProperty(key)) {
+			heap[key].push(value);
+		}
+		else {
+			heap[key] = [value];
+		}
+
+		return heap;
+	}, {});
+
+	return Object.keys(heap).map(function(key) {
+		return buildBundle(key, heap[key]);
+	});
+}
